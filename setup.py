6c897821a17d5fc175e3a1e8f37d19ebf63a4494
Vaishali Ingle <v.ingle1@zensar.com>
2022-11-30 12:56:42 +0530

    Adding Cookiecutter Template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='endtoenddemo',
+    author='vaishali ingle',
+    license='MIT',
+)
